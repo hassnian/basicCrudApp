baa335cb726ec0335e0ed042f3143e5a4c4a042d
Hassnian <hassnianxd@gmail.com>
2019-01-12 11:28:01 +0100

    add Countrie created

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
-// Page loaded
-function load(json){
+// When page is loaded
+window.addEventListener('DOMContentLoaded',load());
 
-    console.log(json);
+
+
+// request
+function load(){
+    const request = new XMLHttpRequest();
+    request.open("get", "data.json");
+    request.onload = () => {
+        try{
+            const json=JSON.parse(request.responseText);
+            // console.log(json);
+            UI.addCountrie(json);
+            
+        }
+        catch(e){
+            console.warn("no se pudo cargar la página");
+            
+        }
+        
+    }
+    request.send();
+    request.responseText;
 
 }
 
-const request = new XMLHttpRequest();
-request.open("get", "data.json");
-request.onload = () => {
-    const json=JSON.parse(request.responseText);
-    load(json);
+// UI
+class UI{
+    static addCountrie(json){
+        
+        const tbody=document.querySelector('#countries');
+        json.forEach(cell => {
+            UI.counter(this);
+            const row=document.createElement('tr');
+            row.innerHTML = `
+        <td>${cell}</td>
+        <td><input type="button" value="Edit" class="btn btn-light edit"></td>
+        <td><input type="button" value="Delete" class="btn btn-light delete"></td>`;
+        tbody.appendChild(row);
+            console.log(cell);
+        });
+
+    }
+// Counter of countries
+    static counter(json){
+        const counter=document.querySelector('#counter');
+        // counter.innerHTML("hello");
+        console.log(counter);
+    }
+
 }
-request.send();
-request.responseText;
